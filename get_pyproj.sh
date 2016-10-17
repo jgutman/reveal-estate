3ea7065ede843088cc79c4d913d7f8d85bbb0b30
Jacqueline Gutman <jacqueline.gutman@gmail.com>
2016-10-17 12:39:11 -0400

    update pyproj repo installation instructions/reqs

---
+++ get_pyproj.sh
@@ -0,0 +1,11 @@
+# Get PyProj repository to perform cartographic transformations from
+# geographic (longitude,latitude) to native map projection (x,y) coordinates
+# and vice versa using the Proj class
+
+git clone https://github.com/jswhit/pyproj
+cd pyproj
+python3 setup.py build
+python3 setup.py install
+# runs unit tests to check install
+# if this doesn't work, run `pip3 install nose2` and retry
+nose2
