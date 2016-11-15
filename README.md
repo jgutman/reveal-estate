f0c59e9920fe8206317bad81a8abab63c2359d05
Jacqueline Gutman <jacqueline.gutman@gmail.com>
2016-11-15 18:55:18 -0500

    starting regression loop function to run through all models

--- README.md
+++ README.md
@@ -11,6 +11,6 @@ Python version 3.5.2
 
 * Download PyProj github repository to perform transformations between X/Y coordinates and latitude/longitude coordinates by running `source get_pyproj.sh`.
 
-* Merge PLUTO and Department of Finance data for specified years and boroughs by running `python3 merge_pluto_finance.py` to obtain and merge data for default boroughs and years (Manhattan and Brooklyn, 2014-2015), or by running `python3 merge_pluto_finance.py --borough {BOROUGH} {BOROUGH} --year {YEAR} {YEAR}` for whatever boroughs and years desired. See `python3 merge_pluto_finance.py --help` for details.
+* Merge PLUTO and Department of Finance data for specified years and boroughs by running `python3 merge_pluto_finance_new.py` to obtain and merge data for default boroughs and years (Manhattan and Brooklyn, 2014-2015), or by running `python3 merge_pluto_finance_new.py --borough {BOROUGH} {BOROUGH} --year {YEAR} {YEAR}` for whatever boroughs and years desired. See `python3 merge_pluto_finance_new.py --help` for details.
 
 * Fit Linear Regression or Random Forest Model to merged data set by running `python3 final_modeling.py --data {path_to_merged_csv} --model {LR or RF}`. See `python3 final_modeling.py --help` for details.
---
+++ regression_loop.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+import random
+import pylab as pl
+import matplotlib.pyplot as plt
+from scipy import optimize
+import time
+from sklearn import preprocessing, cross_validation, svm, metrics, tree, decomposition
+from sklearn.metrics import *
+from sklearn.preprocessing import StandardScaler
+from sklearn.cross_validation import train_test_split
+from sklearn.grid_search import ParameterGrid
+
+from sklearn.ensemble import AdaBoostRegressor, BaggingRegressor, ExtraTreesRegressor,
+                                    RandomForestRegressor, GradientBoostingRegressor
+from sklearn.linear_model import ElasticNetCV, HuberRegressor, BayesianRidge,
+                                    LassoLarsCV, LassoCV, RidgeCV, SGDRegressor
+from sklearn.svm import SVR, LinearSVR
+
+class Timer(object):
+    def __init__(self, name=None):
+        self.name = name
+
+    def __enter__(self):
+        self.tstart = time.time()
+        return self
+
+    def time_check(self):
+        return time.time() - self.tstart
+
+    def __exit__(self, type, value, traceback):
+        if self.name:
+            print("{}:".format(self.name), end = ' ')
+print('{:.2} seconds elapsed'.format(time.time() - self.tstart))
+
+def define_model_params():
+    mods = {
+
+    }
+
+    params = {
+
+    }
+    return mods, params
+
+def model_loop(models_to_run, mods, params, X_train, X_test, y_train, y_test,
+    models_to_run, criterion = 'mse', cv_folds = 10):
+    """
+    Returns a dictionary where the keys are model nicknames (strings)
+    and the values are regressors with methods predict and fit
+
+    :param dict(str:estimator) mods: models as returned by define_model_params
+    :param dict(str:dict) params: grid of regressor hyperparameter options
+        to grid search over as returned by define_model_params
+    :param pandas.DataFrame X_train: training features for model
+    :param pandas.DataFrame X_test: data to predict with same features as X_train
+    :param pandas.Series y_train: Target variable for X_train
+    :param pandas.Series y_test: Target variable for X_test
+    :param list[string] models_to_run: which models to actually run
+        (e.g. ['ridge', 'RF'])
+    :param string criterion: evaluation criterion for model selection on the
+        validation set, (e.g. 'mse')
+    """
+    with Timer('model comparison loop') as qq:
