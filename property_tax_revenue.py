26f27bb49994a9cdc1f719eacb55df06df464567
Jacqueline Gutman <jacqueline.gutman@gmail.com>
2016-12-14 21:56:59 -0500

    fix bbl extract and match in tax revenue

--- property_tax_revenue.py
+++ property_tax_revenue.py
@@ -2,7 +2,6 @@ from argparse import ArgumentParser
 import pandas as pd
 import numpy as np
 import final_data_clean as dc
-import final_modeling as fm
 
 
 def main():
@@ -14,19 +13,19 @@ def main():
         help="Path to merged data used to train the model.")
     parser.set_defaults(datapath = "data/results/price_increase_rf.csv",
         bbl_path = "data/subway_bbls/QueensLightrail_full1.csv",
-        data_original = \
-        "data/merged/bronx_brooklyn_manhattan_queens_statenisland_2003_2016")
+        data_original = "{}/{}.csv".format("data/merged",
+        "bronx_brooklyn_manhattan_queens_statenisland_2003_2016"))
 
     args = parser.parse_args()
     datapath, data_original, bbl_path = args.datapath,
         args.data_original, args.bbl_path
 
-    data = pd.read_csv(datapath)
-    data_with_bbl = pd.read_csv(data_path, low_memory = True)
-    data_with_bbl = data_with_bbl.loc[['bbl', 'sale_year',
-        'sale_price', 'price_per_sqft']]
+    price_increase = pd.read_csv(datapath)
+    data_with_bbl = pd.read_csv(data_original, usecols = ['bbl', 'sale_year',
+        'sale_price', 'price_per_sqft', 'subwaydist'])
     affected_properties, _, bbls = dc.extract_affected_properties(
         data_with_bbl, bbl_path)
+    affected_properties['bbl'] = bbls
 
 
 if __name__ == '__main__':
